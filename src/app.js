5ab065f8bfb733a3a1367c5f58b8ae06dae3a835
Desarrollos MJA <desarrollos.mja@gmail.com>
2022-05-16 12:41:35 -0300

    Server config

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
 const express = require("express")
 const app = express()
+const { config } = require("./config/config.js")
+const cors = require("cors")
+const server = app.listen(config.PORT, () => console.log(`Listening on http://localhost:${config.PORT}`))
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(cors("*"))
---
+++ src/config/config.js
@@ -0,0 +1,7 @@
+require("dotenv").config()
+
+const config = {
+    PORT: process.env.PORT || 8080
+}
+
+module.exports = { config }
